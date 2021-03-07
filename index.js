bd41db1faf2de72b4155a123673639e73c3efe60
taylorsieling <taylorasieling@gmail.com>
2021-03-07 15:41:58 -0600

    commit tree 748b991083f48a971c1e1aafa45593d27b8b37d9

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Add your code here
+
+function submitData(name, email) {
+
+    const formData = {
+        name: name,
+        email: email
+    }
+
+    const configObj= {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "Accept": "application/json"
+        },
+        body: JSON.stringify(formData)
+      };
+
+    return fetch("http://localhost:3000/users", configObj)
+      .then(function(response) {
+        return response.json();
+      })
+      .then(function(object) {
+          console.log(object);
+          const h3 = document.createElement('h3')
+          h3.innerHTML = object.id
+          document.body.appendChild(h3)
+      })
+      .catch(function(error) {
+          console.log(error.message);
+          const p = document.createElement('p')
+          p.innerHTML = error.message
+          document.body.appendChild(p)
+      });
+
+}
